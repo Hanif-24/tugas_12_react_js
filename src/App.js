21299a268b45131df61dac3928a332fd16bbdb29
Hanif-24 <m.alihanif249@gmail.com>
2022-10-27 00:01:29 +0700

    12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+
+import { Grid,Checkbox, Form, Button, Header, Image, Segment,Message } from 'semantic-ui-react'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column width={6}>
+         <Form>
+            <Header as='h2' color='teal'>
+              <Image bordered src='https://s3-ap-southeast-1.amazonaws.com/niomic/img-v1/c_login_logo.png' /> 
+              Member Login
+            </Header>
+            <Segment stacked>
+              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+              <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password'/>
+              <Form.Field>
+                <Checkbox label='I agree to the Terms and Conditions' />
+              </Form.Field>
+              <Button type='submit'color='teal' fluid>
+                
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+              Tidak Punya Akun ? Silahkan <a href="/"> Register</a>
+            </Message>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
